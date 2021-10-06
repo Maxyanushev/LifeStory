08810f4aab6c65b53136d1d47bba9f40acadba08
Максим <max.samurai.04@gmail.com>
2021-10-06 21:14:47 +0300

    start created project: add background music and navigationDraver

--- app/src/main/java/com/example/nana/ui/studies/StudiesViewModel.java
+++ app/src/main/java/com/example/nana/ui/studies/StudiesViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class StudiesViewModel extends ViewModel {
+
     private MutableLiveData<String> mText;
 
     public StudiesViewModel() {

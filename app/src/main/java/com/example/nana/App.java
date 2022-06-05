7007fc403bb73f15206cb008f715ea388681d48c
Максим <max.samurai.04@gmail.com>
2022-06-05 22:16:19 +0200

    add icon and some changes in manifest and gradle

---
+++ app/src/main/java/com/example/nana/App.java
@@ -0,0 +1,8 @@
+package com.example.nana;
+
+import androidx.lifecycle.LifecycleObserver;
+import androidx.multidex.MultiDexApplication;
+
+public class App extends MultiDexApplication implements LifecycleObserver {
+
+}

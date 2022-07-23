6578d9edb0037a0e3181695117f2cdf8531afa78
Максим <max.samurai.04@gmail.com>
2022-07-23 10:43:46 +0200

    add base ui for PublicationActivity.java

--- app/src/main/java/com/example/nana/publication/PublicationActivity.java
+++ app/src/main/java/com/example/nana/publication/PublicationActivity.java
@@ -1,9 +1,11 @@
 package com.example.nana.publication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nana.MainActivity;
 import com.example.nana.R;
 import com.example.nana.databinding.ActivityPublicationBinding;
 
@@ -21,7 +23,23 @@ public class PublicationActivity extends AppCompatActivity {
         ActivityPublicationBinding binding = ActivityPublicationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.searchView.setIconified(false);
-        binding.searchView.onActionViewExpanded();
+        binding.btnBack.setOnClickListener(v -> onActivity("MainActivity"));
+
+        binding.btnHelp.setOnClickListener(v -> onActivity("DetailPublicationActivity"));
+    }
+
+    public void onActivity(String o) {
+        Intent intent;
+        switch (o) {
+            case "MainActivity":
+                intent = new Intent(this, MainActivity.class);
+                break;
+            case "DetailPublicationActivity":
+                intent = new Intent(this, DetailPublicationActivity.class);
+                break;
+            default:
+                intent = new Intent(this, PublicationActivity.class);
+                break;
+        } startActivity(intent);
     }
 }

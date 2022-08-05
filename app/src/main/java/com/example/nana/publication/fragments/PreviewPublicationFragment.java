a62808cc32207d976d296ff6b07406520994e851
Максим <max.samurai.04@gmail.com>
2022-08-05 23:07:29 +0200

    make send data from PublicationFragment to PreviewPublicationFragment without optimization

--- app/src/main/java/com/example/nana/publication/fragments/PreviewPublicationFragment.java
+++ app/src/main/java/com/example/nana/publication/fragments/PreviewPublicationFragment.java
@@ -3,13 +3,20 @@ package com.example.nana.publication.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.nana.R;
 import com.example.nana.core.BaseFragment;
 import com.example.nana.databinding.FragmentPreviewPublicationBinding;
+import com.example.nana.databinding.FragmentPublicationBinding;
+
+import org.w3c.dom.Text;
 
 public class PreviewPublicationFragment extends BaseFragment {
 
@@ -18,6 +25,20 @@ public class PreviewPublicationFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPreviewPublicationBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getParentFragmentManager().setFragmentResultListener("dataFromPublication", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String data = result.getString("MyArg");
+                binding.include.textView6.setText(data);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nana/publication/fragments/PublicationFragment.java
+++ app/src/main/java/com/example/nana/publication/fragments/PublicationFragment.java
@@ -3,11 +3,16 @@ package com.example.nana.publication.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.nana.R;
 import com.example.nana.core.BaseFragment;
@@ -29,12 +34,24 @@ public class PublicationFragment extends BaseFragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
     private void init() {
 
     }
 
     private void initListeners() {
         binding.cardButtonNext.setOnClickListener(v -> {
+
+            Bundle bundle = new Bundle();
+            String text = binding.etEnterText.getText().toString();
+            bundle.putString("MyArg", text);
+            getParentFragmentManager().setFragmentResult("dataFromPublication", bundle);
+
             FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction();
             fragmentTransaction.replace(R.id.frame_layout, previewPublicationFragment);
             fragmentTransaction.commit();
